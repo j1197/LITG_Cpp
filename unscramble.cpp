b277b8d550545a5e2b980a9aaa4b8967c4af172a
J <jehlum11@gmail.com>
2019-05-30 09:03:13 +0530

    Code for game: Unscrambling

--- unscramble.cpp
+++ unscramble.cpp
@@ -6,11 +6,14 @@
  * This file contains code for game Unscramble
  */
 
-// reading a text file
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <list>
+#include<cstdlib>
+#include<ctime>
+#include <vector>
 
 using namespace std;
 
@@ -28,7 +31,7 @@ list <string> create_list(){
     string line;
     list <string> main_list;
 
-    ifstream myfile ("/Users/siddhantbansal/Desktop/Hitler_CPP/words.txt");
+    ifstream myfile ("/Users/jp/Desktop/words.txt");
     if (myfile.is_open())
     {
         while ( getline (myfile,line) )
@@ -48,6 +51,38 @@ list <string> create_list(){
 int main () {
     list <string> list_words;
     list_words = create_list();
-    showlist(list_words);
+    //showlist(list_words);
+    srand(time(0));
+    int random = rand();
+    int min = 0;
+    int max = 44000;
+    cout << "Seed = " << time(0) << endl;
+    int finalNum = rand()%(max-min+1)+min;
+    cout << "Random number = " << finalNum << endl;
+    vector<string> words;
+    ifstream file("/Users/jp/Desktop/words.txt");
+    string line;
+    while (getline(file, line)) words.push_back(line);
+    string a = words[rand() % words.size()];
+    string b = a;
+    //cout << b << endl;
+    cout << a << endl;
+    random_shuffle(a.begin(), a.end());
+    cout << a << '\n';
+
+    cout << b << endl;
+    cout << "Enter your answer: " << endl;
+    string guessedword;
+
+    getline( cin, guessedword);
+
+    //std::transform(guessedword.begin(), guessedword.end(), guessedword.begin(), ::tolower);
+
+    if (b == guessedword)
+        cout << "You are right! " << endl;
+    else
+        cout << " Wrong answer, the word was: " << b << endl;
+
     return 0;
+
 }
